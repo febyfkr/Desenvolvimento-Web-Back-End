cb407e6c3cfe381d35aebb26b8d898bc1acab8c4
Feby Kouyoumdjian Reis <159589782+febyfkr@users.noreply.github.com>
2026-03-27 18:18:59 -0300

    Add files via upload

---
+++ Controle de Alunos/projeto/src/main/java/com/aula/projeto/ProjetoApplication.java
@@ -0,0 +1,14 @@
+package com.example.controle_de_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeAlunosApplication.class, args);
+	
+	}
+
+}
