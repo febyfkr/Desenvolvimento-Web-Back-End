7a1904a66b98f0cd1701e04c76a58d5210a04b3c
Feby Kouyoumdjian Reis <159589782+febyfkr@users.noreply.github.com>
2026-04-17 19:23:36 -0300

    Delete Atividades - Services e Controller/projeto directory

--- Atividades - Services e Controller/projeto/src/main/java/com/aula/projeto/repository/ConvenioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.aula.projeto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.aula.projeto.model.Convenio;
-
-public interface ConvenioRepository extends JpaRepository<Convenio, Long> {}
